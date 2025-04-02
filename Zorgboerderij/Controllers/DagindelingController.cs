8e57b5a6a033a346087432f507497fa133f51c13
Bodhi van Lenthe <bodhi.lenthe@student.graafschapcollege.nl>
2025-04-02 08:54:05 +0200

    dagindeling + navigation

---
+++ Zorgboerderij/Controllers/DagindelingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Zorgboerderij.Controllers
+{
+    public class DagindelingController : Controller
+    {
+        public IActionResult Dagindeling()
+        {
+            return View();
+        }
+    }
+}
